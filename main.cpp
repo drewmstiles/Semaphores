3ff852b5fc1e6bb9d81d692196c43b63ad95fd46
Drew Stiles <drewmstiles@gmail.com>
2015-11-30 16:11:56 -0800

    Finishes deposit and withdrawal requests

--- main.cpp
+++ main.cpp
@@ -41,11 +41,8 @@ static const int REQS = 2000;
 // the number of bank accounts
 static const int NUM_ACCTS = 4;
 
-// the semaphore id
-// static const int AVAILABLE = 1;
-// 
-// The index in shared memory buffers for Checking account
-// static const int CHECKING = 0;
+// the amount exchanged during a transaction
+static const int PAYMENT = 1;
 
 /*
  *  Service requests for user processes
@@ -76,7 +73,7 @@ static const int SAV_SEM = 1;
 static const int VAC_SEM = 2;
 static const int IRA_SEM = 3;
 
-static const int semID[4] = {CHK_SEM, SAV_SEM, VAC_SEM, IRA_SEM};
+static const int locks[4] = {CHK_SEM, SAV_SEM, VAC_SEM, IRA_SEM};
  
  
  /*
@@ -89,6 +86,8 @@ static const int VAC_ACCT = 2;
 static const int IRA_ACCT = 3;
 
 
+static const char* names[4] = { "Checkings", "Savings", "Vacation", "IRA" };
+
 // Summary:
 //
 //  Returns one of the five service requests.
@@ -97,12 +96,13 @@ static const int IRA_ACCT = 3;
 //
 //      an integer denoting the service to request
 //
-int getRandomRequest(default_random_engine, uniform_int_distribution<int>);
+int randomRequest();
 
-void deposit( int * account);
+void deposit(int *account);
 
+void withdraw(int *account);
 
-int getRandomAccount();
+int randomAccount();
 
 
 /*
@@ -143,16 +143,39 @@ int main(int argc, const char * argv[]) {
         	 * Execute with and without P and V operations to observe corruption
         	 */
            
-            switch(1)
+           	int request = randomRequest();
+            switch(request)
             {
                 case 1: {
-                    int account = getRandomAccount();
-                    sem.P(semID[account]);
+                    int account = randomAccount();
+                    sem.P(locks[account]);
+                    printf("Deposit to %s account\n", names[account]);
                     deposit(bank[account]);
-                    sem.V(semID[account]);
+                    sem.V(locks[account]);
                     break;
                 }
+                case 2: {
+               	 	int account = randomAccount();
+               	 	sem.P(locks[account]);
+               	 	printf("Withdraw from %s account\n", names[account]);
+               	 	withdraw(bank[account]);
+               	 	sem.V(locks[account]);
+                	break;
+                }
+                case 3: {
+                	printf("request = %d ", request);
+                	break;
+                }
+                case 4: {
+                	printf("request = %d ", request);
+                	break;
+                }
+                case 5: {
+					printf("request = %d ", request);                	
+					break;
+				}
                 default: {
+                	printf("ERROR - Default on request = %d\n", request);
                     break;
                 }
             }
@@ -169,36 +192,35 @@ int main(int argc, const char * argv[]) {
 	while (p > 0) {
 	  pid = wait(&status);
 	  printf("pid (%d) exits\n", pid);
-	  --p; // decrement number of running children
+	  --p;
 	}
 	
-	// ensure that total equals number of processes
-    int x;
-    for (x = 0; x < 4; x++){
-        printf("$%d in Checking account\n", *bank[x]);
+	// output and clean up
+    for (int x = 0; x < NUM_ACCTS; x++){
+        printf("$%d in %s account\n", *bank[x], names[x]);
+    	shmctl(shmid[x], IPC_RMID, NULL);
     }
-	// clean up
-	for(int x = 0; x < NUM_ACCTS; x++){
-       	shmctl(shmid[x], IPC_RMID, NULL);
-    } 
     
 	sem.remove();
 	
     exit(0);
 }
 
-int getRandomAccount()
+int randomAccount()
 {
 	default_random_engine generator(random_device{}());
 	uniform_int_distribution<int> distribution(0, NUM_ACCTS - 1);
 	return distribution(generator);
 }
 
-int getRandomRequests(default_random_engine generator, uniform_int_distribution<int> distribution) {
+int randomRequest() {
 
+ 	default_random_engine generator(random_device{}());
+ 	uniform_int_distribution<int> distribution(0, pow(2.0, 32.0) - 1.0);
+ 	
 	// TODO This rarely generates 5 as the return type
 	while (true) {
-		 int random = distribution(generator);
+		int random = distribution(generator);
 		if (random % ADD == 0) {
 			return 1;
 		} else if (random % REM == 0) {
@@ -217,13 +239,25 @@ int getRandomRequests(default_random_engine generator, uniform_int_distribution<
 
 
 
-void deposit(int * account) {
-    
+void deposit(int *account) {
     int bal = *account;
     usleep(ONE_MS);
     *account = (bal + 1);
 
 }
+
+void withdraw(int *account) {
+	int oldBalance = *account;
+	int newBalance = oldBalance - PAYMENT;
+	if (newBalance < 0) {
+// 		printf("Withdraw Notice - Insufficient Funds (BAL = $%d, REQ = $%d)\n", 
+// 			oldBalance, PAYMENT);
+	} 
+	else {
+		*account = newBalance;
+	}
+}
+		
 
 
 // This code tests the distribution of random bank requests
